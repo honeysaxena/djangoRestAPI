52b87f7ab5fd32b0aac92b04fd9092f2c4649f57
Honey <honeysaxena525@gmail.com>
2023-05-07 02:48:51 +0530

    complexity added its getting really difficult

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@ install:
 	pip install --upgrade pip && pip install -r requirements.txt
 format:
 	#format code
-	#black *.py mylib/*.py	
+	black backend/*.py py_client/*.py	
 lint:
 	#flake8 or #pylint
-	#pylint --disable=R,C *.py mylib/*.py
+	pylint --disable=R,C backend/*.py py_client/*.py
 test:
 	#test
 	#python -m pytest -vv --cov==mylib --cov==main test_*.py
---
+++ py_client/basic.py
@@ -0,0 +1,13 @@
+import requests
+
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api"
+
+get_response = requests.get(endpoint, json={"content": "Hello world"})
+# print(get_response.status_code)
+# print(get_response.text)
+# print('')
+print(get_response.json())
+# print('')
+# print(get_response.status_code)
